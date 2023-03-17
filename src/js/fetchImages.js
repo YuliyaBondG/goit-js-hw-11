42b65c3ed6f3cba60617bfb41855e76709e5b905
YuliyaBondG <manyunka@ukr.net>
2023-03-17 18:54:36 +0200

    task

--- src/js/fetchImages.js
+++ src/js/fetchImages.js
@@ -1,19 +1,19 @@
-import axios from "axios";
+import axios from 'axios';
 
 export const fetchImages = async (inputValue, pageNr) => {
-    return fetch(
-        `https://pixabay.com/api/?key=34474305-59cca09f4f729e737f986d19f=${inputValue}&orientation=horizontal&safesearch=true&image_type=photo&per_page=40&page=${pageNr}`
-    )
+  return fetch(
+    `https://pixabay.com/api/?key=34474305-59cca09f4f729e737f986d19f&q=${inputValue}&orientation=horizontal&safesearch=true&image_type=photo&per_page=40&page=${pageNr}`
+  )
     .then(response => {
-        if (!response.ok) {
-            if (response.status === 404) {
-                return [];
-            }
-            throw new Error(response.status);
+      if (!response.ok) {
+        if (response.status === 404) {
+          return [];
         }
-        return response.json();
+        throw new Error(response.status);
+      }
+      return response.json();
     })
     .catch(error => {
-        console.error(error);
+      console.error(error);
     });
 };
